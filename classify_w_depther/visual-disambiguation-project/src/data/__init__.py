48932a06166c69453b2517f0dc8cc2384f285ae5
Duonn512 <duo5122004@gmail.com>
2024-12-25 12:59:17 +0700

    remove unecessary folders

--- classify_w_depther/visual-disambiguation-project/src/data/__init__.py
+++
@@ -1,3 +0,0 @@
-# Contents of /visual-disambiguation-project/visual-disambiguation-project/src/data/__init__.py
-
-# This file is intentionally left blank.
--- classify_w_depther/visual-disambiguation-project/src/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-# Contents of /visual-disambiguation-project/visual-disambiguation-project/src/utils/__init__.py
-
-# This file is intentionally left blank.
